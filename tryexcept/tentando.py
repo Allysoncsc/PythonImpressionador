9638512aa94afecfe2cf188a330e55488d2be38e
AllysonCS <allysoncsc@gmail.com>
2023-10-03 08:50:37 -0300

    iniciando estatistica com numpy

--- tryexcept/tentando.py
+++ tryexcept/tentando.py
@@ -1,25 +1,34 @@
 
 
 
-a = 18
-b = 0
+# a = 18
+# b = 0
 
 
-try:
-  c = a/b
-except ZeroDivisionError:
-  print('tentou dividir por zero')
-except NameError:
-  print('Variavel não definida')
-except Exception:
-  print('ERRO desconhecido')
-
-
-
+# try:
+#   c = a/b
+# except ZeroDivisionError:
+#   print('tentou dividir por zero')
+# except NameError:
+#   print('Variavel não definida')
+# except Exception:
+#   print('ERRO desconhecido')
 
 
 
 
 
-
-
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
+text = '''Hoje é um grande dia.  Está ainda 
+melhor do que ontem. E ontem foi o melhor 
+dia de todos. Amanhã será um dia especial.'''
+print("\nOriginal string:")
+print(text)
+token_text = sent_tokenize(text)
+print("\nSentence-tokenized copy in a list:")
+print(token_text)
+print("\nRead the list:")
+for s in token_text:
+    print(s)
